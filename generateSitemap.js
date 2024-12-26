ee1ca2c79a5cd87add57eec01777148602694738
Nikita Korzhitskii <6025342+nikita-kun@users.noreply.github.com>
2024-12-26 14:34:13 +0100

    Update generateSitemap.js

--- generateSitemap.js
+++ generateSitemap.js
@@ -13,13 +13,13 @@ function generateSitemap() {
 
     document.querySelectorAll('a').forEach(anchor => {
         const href = anchor.getAttribute('href');
-        if (href && !href.includes('#') && (href.startsWith('https://nikita-kun.github.io') || !href.startsWith('http'))) {
-            urls.add(href);
+        if (href && !href.includes('#') && !href.startsWith('http')) {
+            urls.add(`https://nikita-kun.github.io${href}`);
         }
     });
 
     const sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n` +
-        Array.from(urls).map(url => `<url><loc>${url}</loc><changefreq>${url === 'https://example.com/' ? 'daily' : 'weekly'}</changefreq></url>`).join('\n') + `\n</urlset>`;
+        Array.from(urls).map(url => `<url><loc>${url}</loc><changefreq>${url === 'https://nikita-kun.github.io/' ? 'daily' : 'weekly'}</changefreq></url>`).join('\n') + `\n</urlset>`;
 
     const deployDir = path.join(__dirname, 'deploy');
     if (!fs.existsSync(deployDir)) fs.mkdirSync(deployDir);
